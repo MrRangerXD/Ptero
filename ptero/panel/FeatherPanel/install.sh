ef1768014975551d449eb64a4dcd43bb5cb47002
Ｎᴏʙɪᴛᴀ <nn8109239@gmail.com>
2026-01-24 16:29:06 +0530

    Update install.sh

--- ptero/panel/FeatherPanel/install.sh
+++ ptero/panel/FeatherPanel/install.sh
@@ -85,6 +85,8 @@ curl -sS https://getcomposer.org/installer \
 apt install -y nodejs npm
 npm install -g n
 n lts
+curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash && source ~/.bashrc && nvm install node && nvm alias default node
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
